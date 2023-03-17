268d4be249c02bd255989c5ca1d10aec4796f709
SaschaGoebbels <109481671+SaschaGo2003@users.noreply.github.com>
2023-03-17 21:32:54 +0100

    update

--- src/utils/loginLogic.js
+++ src/utils/loginLogic.js
@@ -6,14 +6,27 @@ import { state } from '../components/store/state';
 export const createAcc = async (url, user, infoBox) => {
   let res;
   state.loading = true;
+
   try {
-    await fetch(url, {
-      method: 'POST',
-      // headers: { 'Content-type': 'application/json; charset=UTF-8' },
-      // body: JSON.stringify(user),
-    })
-      .then(response => response.json())
+    let req = new Request(url, {
+      // method: 'POST',
+      mode: 'cors', //just a safe-guard indicating our intentions of what to allow
+      credentials: 'include', //when will the cookies and authorization header be sent
+    });
+    await fetch(req)
+      .then(resp => {
+        let cookie = resp.headers.get('set-cookie');
+        console.log('set-cookie header value', cookie);
+        return resp.json();
+      })
       .then(json => (res = json));
+    // // // await fetch(url, {
+    // // //   method: 'POST',
+    // // //   // headers: { 'Content-type': 'application/json; charset=UTF-8' },
+    // // //   // body: JSON.stringify(user),
+    // // // })
+    // // //   .then(response => response.json())
+    // // //   .then(json => (res = json));
   } catch (err) {
     console.log('❌', err);
   }
@@ -25,10 +38,34 @@ export const login = async (url, email, password, infoBox) => {
   state.loading = true;
   console.log('✅', url, email, password, infoBox);
   let res;
+  const baseURL = 'http://localhost:5555/set'; // cross-site CORS
   try {
+    // // let req = new Request(url, {
+    // //   // mode: 'cors', //just a safe-guard indicating our intentions of what to allow
+    // //   // credentials: 'include', //when will the cookies and authorization header be sent
+    // //   method: 'POST',
+    // //   mode: 'cors',
+    // //   credentials: 'include',
+    // //   headers: {
+    // //     'Content-Type': 'application/json',
+    // //   },
+    // //   body: JSON.stringify({
+    // //     name: 'John Doe',
+    // //     email: 'johndoe@example.com',
+    // //   }),
+    // // });
+    // // await fetch(req)
+    // //   .then(resp => {
+    // //     let cookie = resp.headers.get('set-cookie');
+    // //     console.log('LOGIN set-cookie header value', cookie);
+    // //     return resp.json();
+    // //   })
+    // //   .then(json => (res = json));
+    //
+    //
     await fetch(url, {
-      method: 'POST',
       credentials: 'include',
+      method: 'POST',
       mode: 'cors',
       headers: { 'Content-type': 'application/json; charset=UTF-8' },
       body: JSON.stringify({
